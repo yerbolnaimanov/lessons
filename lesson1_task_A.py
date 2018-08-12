5a3098dee50f5d2ed1914b1437ceeec3a1153e4b
yerbolnaimanov <42176407+yerbolnaimanov@users.noreply.github.com>
2018-08-12 16:32:23 +0600

    adding chouce btwn r and d

--- lesson1_task_A.py
+++ lesson1_task_A.py
@@ -2,8 +2,13 @@ import math
 def getLength():
     length=-1
     try:
-        radius = int(input("ВВЕДИТЕ РАДИУС: "))
-        length=2*math.pi*radius
+        type = input("ВВЕДИТЕ БУКВУ R или D:")
+        if type=="R":
+            radius = int(input("ВВЕДИТЕ РАДИУС: "))
+            length=2*math.pi*radius
+        elif type=="D":
+            diam = int(input("ВВЕДИТЕ ДИАМЕТР: "))
+            length = math.pi * diam
     except Exception as e:
         print(e)
     return round(length, 2)
