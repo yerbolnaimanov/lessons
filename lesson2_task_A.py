8b06535ec0de613c4bb073e095b3382e0e8fb958
yerbolnaimanov <42176407+yerbolnaimanov@users.noreply.github.com>
2018-08-12 18:33:40 +0600

    added lesson 2 Task A

--- lesson2_task_A.py
+++ lesson2_task_A.py
@@ -1,4 +1,15 @@
 # урок 2, задание А
+#subtask1
 fio = input("Как Вас зовут? ")
 fio=fio.strip(' !,./?!@#$%^&*()_+=~\"')
 print("Здравствуйте {0}, как ваше настроение?".format(fio))
+
+#subtask2
+number = 0
+try:
+    number = float(input("Введите число: "))
+except:
+    print("ошибка")
+
+print("В процентах: "+'{:.2%}'.format(number))
+print("В валюте: "+ '{:.2f}'.format(number)+"$")
